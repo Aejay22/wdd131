35781918209d65f5b284b0151fe885bb82e3e17b
Aejay22 <janthony2@byupathway.edu>
2026-09-12 00:34:31 +0100

    commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,17 @@ function show(){
 }
 
 menu.addEventListener("click", show);
+
+var yearSpan = document.getElementById("currentyear");
+var modifiedSpan = document.getElementById("lastModified");
+
+var today = new Date();
+var currentYear = today.getFullYear();
+
+yearSpan.textContent = currentYear;
+
+var lastModifiedDate = document.lastModified;
+
+modifiedSpan.textContent = "Last Modification: " + lastModifiedDate;
+
+
